d0675fe93987808a612d14e9982eadfbd013c571
Ivan Cvitkovic <ivancvitkovic98@gmail.com>
2020-10-07 12:44:36 +0200

    Add and display todos

--- App.js
+++ App.js
@@ -1,12 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, FlatList } from "react-native";
+import Header from "./components/Header";
+import TodoItem from "./components/TodoItem";
+import AddItem from "./components/AddItem";
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    { text: "buy coffee", key: "1" },
+    { text: "create an app", key: "2" },
+    { text: "smaraj budu", key: "3" },
+  ]);
+
+  const addItem = (text) => {
+    setTodos((prevTodos) => {
+      return [{ text, key: Math.random().toString() }, ...prevTodos];
+    });
+  };
+
+  const deleteItem = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((x) => x.key !== key);
+    });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header />
+      <View style={styles.content}>
+        <AddItem addItem={addItem} />
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => (
+              <TodoItem item={item} deleteItem={deleteItem} />
+            )}
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -14,8 +44,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+  content: {
+    padding: 40,
+  },
+  list: {
+    marginTop: 20,
   },
 });
