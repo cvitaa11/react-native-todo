85c5f6a09b6c0591309d2cd291f390a6f17b5704
Ivan Cvitkovic <ivancvitkovic98@gmail.com>
2020-10-07 14:49:06 +0200

    added expo icons

--- components/TodoItem.js
+++ components/TodoItem.js
@@ -1,10 +1,14 @@
 import React from "react";
-import { StyleSheet, Text, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
 
 export default function TodoItem({ item, deleteItem }) {
   return (
     <TouchableOpacity onPress={() => deleteItem(item.key)}>
-      <Text style={styles.item}>{item.text}</Text>
+      <View style={styles.item}>
+        <MaterialIcons name="delete" size={18} color="black" />
+        <Text style={styles.itemText}>{item.text}</Text>
+      </View>
     </TouchableOpacity>
   );
 }
@@ -17,5 +21,9 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderStyle: "dashed",
     borderRadius: 10,
+    flexDirection: "row",
+  },
+  itemText: {
+    marginLeft: 10,
   },
 });
